d6295451a2316ae0bb8e0f8244f2520b740c143c
kgryte <kgryte@gmail.com>
2015-12-31 17:11:18 -0800

    Add tests for get and list

---
+++ test/test.get.js
@@ -0,0 +1,90 @@
+'use strict';
+
+// MODULES //
+
+var test = require( 'tape' );
+var assert = require( 'chai' ).assert;
+var proxyquire = require( 'proxyquire' );
+var get = require( './../lib/get.js' );
+
+
+// FUNCTIONS //
+
+function getOpts() {
+	var opts = {
+		'username': 'kgryte',
+		'registry': 'registry.npmjs.org',
+		'port': 80,
+		'protocol': 'http'
+	};
+	return opts;
+}
+
+
+// FIXTURES //
+
+var data = require( './fixtures/data.json' );
+
+
+// TESTS //
+
+test( 'file exports a function', function test( t ) {
+	t.ok( typeof get === 'function', 'export is a function' );
+	t.end();
+});
+
+test( 'function returns an error to a provided callback if an error is encountered when fetching a package list', function test( t ) {
+	var opts;
+	var get;
+
+	get = proxyquire( './../lib/get.js', {
+		'./request.js': request
+	});
+
+	opts = getOpts();
+	get( opts, done );
+
+	function request( opts, clbk ) {
+		setTimeout( onTimeout, 0 );
+		function onTimeout() {
+			clbk( new Error( 'beep' ) );
+		}
+	}
+
+	function done( error ) {
+		t.ok( error instanceof Error );
+		t.equal( error.message, 'beep' );
+		t.end();
+	}
+});
+
+test( 'function returns a package list to a provided callback', function test( t ) {
+	var expected;
+	var opts;
+	var get;
+
+	get = proxyquire( './../lib/get.js', {
+		'./request.js': request
+	});
+
+	expected = [
+		'beep',
+		'boop'
+	];
+
+	opts = getOpts();
+	get( opts, done );
+
+	function request( opts, clbk ) {
+		setTimeout( onTimeout, 0 );
+		function onTimeout() {
+			clbk( null, data );
+		}
+	}
+
+	function done( error, data ) {
+		assert.deepEqual( data, expected );
+		t.ok( true, 'deep equal' );
+		t.end();
+	}
+});
---
+++ test/test.ls.js
@@ -0,0 +1,97 @@
+'use strict';
+
+// MODULES //
+
+var test = require( 'tape' );
+var assert = require( 'chai' ).assert;
+var proxyquire = require( 'proxyquire' );
+var list = require( './../lib/ls.js' );
+
+
+// FUNCTIONS //
+
+function getOpts() {
+	var opts = {
+		'username': 'kgryte',
+		'registry': 'registry.npmjs.org',
+		'port': 80,
+		'protocol': 'http'
+	};
+	return opts;
+}
+
+
+// FIXTURES //
+
+var data = [
+	'beep',
+	'boop'
+];
+
+
+// TESTS //
+
+test( 'file exports a function', function test( t ) {
+	t.ok( typeof list === 'function', 'export is a function' );
+	t.end();
+});
+
+test( 'function returns an error to a provided callback if an error is encountered when fetching a package list', function test( t ) {
+	var opts;
+	var list;
+
+	list = proxyquire( './../lib/ls.js', {
+		'./factory.js': factory
+	});
+
+	opts = getOpts();
+	list( opts, done );
+
+	function factory( opts, clbk ) {
+		return function list() {
+			setTimeout( onTimeout, 0 );
+			function onTimeout() {
+				clbk( new Error( 'beep' ) );
+			}
+		};
+	}
+
+	function done( error ) {
+		t.ok( error instanceof Error );
+		t.equal( error.message, 'beep' );
+		t.end();
+	}
+});
+
+test( 'function returns a package list to a provided callback', function test( t ) {
+	var expected;
+	var opts;
+	var list;
+
+	list = proxyquire( './../lib/ls.js', {
+		'./factory.js': factory
+	});
+
+	expected = [
+		'beep',
+		'boop'
+	];
+
+	opts = getOpts();
+	list( opts, done );
+
+	function factory( opts, clbk ) {
+		return function list() {
+			setTimeout( onTimeout, 0 );
+			function onTimeout() {
+				clbk( null, data );
+			}
+		};
+	}
+
+	function done( error, data ) {
+		assert.deepEqual( data, expected );
+		t.ok( true, 'deep equal' );
+		t.end();
+	}
+});
